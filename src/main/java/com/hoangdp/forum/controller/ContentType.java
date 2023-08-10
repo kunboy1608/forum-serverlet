cc7cd08c520151df97dd51c6ca7edffc6395932d
hoangdp <kunboy1608@gmail.com>
2023-08-10 21:30:34 +0700

    add hibernate framework

---
+++ src/main/java/com/hoangdp/forum/controller/ContentType.java
@@ -0,0 +1,16 @@
+package com.hoangdp.forum.controller;
+
+public class ContentType {
+    public class Text {
+        public static final String CSS = "text/css";
+        public static final String CSV = "text/csv";
+        public static final String HTML = "text/html";
+        public static final String JAVSCRIPT = "text/javscript";
+        public static final String PLAIN = "text/plan";
+        public static final String XML = "text/xml";
+    }
+
+    public class Application {
+        public static final String JSON = "application/json";
+    }
+}
